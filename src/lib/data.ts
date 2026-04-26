4b5f18bec2a0d2825c7cf1ba3c4c2a8f102e67f6
ToddyHollow <109104615+ToddyHollow@users.noreply.github.com>
2026-04-26 23:32:44 +0000

    updated project title

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -49,7 +49,7 @@ export const skills = {
 
 export const projects = [
   {
-    title: "TCC Final - Architectural Residential Duplex",
+title: "TCC Final - Architectural Residential Duplex",
     github: "",
     pdf: "/pdfs/knodeann-phase6.pdf",
     images: [
